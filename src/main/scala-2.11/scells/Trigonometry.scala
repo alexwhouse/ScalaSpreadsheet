e60b9e39f7309fbe809dbdd76c50ddfc07dc6144
Alex House <alexwhouse@gmail.com>
2015-04-10 00:06:06 -0600

    Added basic trig functions.

---
+++ src/main/scala-2.11/scells/Trigonometry.scala
@@ -0,0 +1,14 @@
+package scells
+
+/**
+ * Basic Trigonometric functions
+ * Created by alexwhouse on 4/9/15.
+ */
+trait Trigonometry {
+  this: Evaluator =>
+  operations +=(
+    "sin" -> { case List(x) => math.sin(x) },
+    "cos" -> { case List(x) => math.cos(x) },
+    "tan" -> { case List(x) => math.tan(x) }
+    )
+}

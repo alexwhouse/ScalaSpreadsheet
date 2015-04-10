f90244f1b0eb47b96b3aff430bfe58cbd298ad23
Alex House <alexwhouse@gmail.com>
2015-04-10 00:22:56 -0600

    Added Pythagorean.

--- src/main/scala-2.11/scells/Trigonometry.scala
+++ src/main/scala-2.11/scells/Trigonometry.scala
@@ -9,6 +9,7 @@ trait Trigonometry {
   operations +=(
     "sin" -> { case List(x) => math.sin(x) },
     "cos" -> { case List(x) => math.cos(x) },
-    "tan" -> { case List(x) => math.tan(x) }
+    "tan" -> { case List(x) => math.tan(x) },
+    "pythagorean" -> { case List(x, y) => math.sqrt(x * x + y * y) }
     )
 }
